c2dce73928a5d0aa6275001e9aff37d01e3a64a5
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-01-13 00:33:12 +0100

    add home page with hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/Home";
 import "./styles/style.scss";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <HomePage />
     </div>
   );
 }
