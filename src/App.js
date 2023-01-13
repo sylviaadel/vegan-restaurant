adbd9e182e00405a964707ed2848e50c7eac16c0
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-01-13 02:16:54 +0100

    Add Category Page static and react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/Home";
+import Category from "./pages/Category";
 import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
 import "./styles/style.scss";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Category" element={<Category />} />
+      </Routes>
       <Footer />
     </div>
   );
