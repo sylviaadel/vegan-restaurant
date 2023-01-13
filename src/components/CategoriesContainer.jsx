1fb79d985d2a8b8b22f07af69e370b6a7186003b
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-01-13 01:53:23 +0100

    enhance names

--- src/components/CategoriesContainer.jsx
+++ src/components/CategoriesContainer.jsx
@@ -1,16 +1,16 @@
-import categories from "../Data/categories.json";
+import category from "../Data/categories.json";
 import arrowRight from "../assets/images/chevron-right.png";
 
-const categoriesItems = categories.map((categories) => (
+const categoriesItems = category.map((category) => (
   <section>
     <div>
       <div className="CategoryImgContainer">
-        <img src={`images/${categories.image}`} alt={categories.alt} />
+        <img src={`images/${category.image}`} alt={category.alt} />
       </div>
-      <h2>{categories.name}</h2>
-      <p>{categories.desc1}</p>
-      <p>{categories.desc2}</p>
-      <a href={categories.link}>
+      <h2>{category.name}</h2>
+      <p>{category.desc1}</p>
+      <p>{category.desc2}</p>
+      <a href={category.link}>
         <img src={arrowRight} alt="Chevron Right" /> View menu
       </a>
     </div>
