97702b109e601605348ffc39de226a2759d18b06
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-01-16 22:23:43 +0100

    add key to category

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -2,7 +2,7 @@ import category from "../Data/categories.json";
 import ProductsContainer from "../components/ProductsContainer";
 
 const categoriesItems = category.map((category) => (
-  <>
+  <div key={category.id}>
     <header className="category-header" key={category.id}>
       <img src={`images/${category.image}`} alt={category.alt} />
       <h1>{category.title}</h1>
@@ -11,7 +11,7 @@ const categoriesItems = category.map((category) => (
       <p>{category.desc}</p>
       <ProductsContainer />
     </div>
-  </>
+  </div>
 ));
 export default function Category(props) {
   return <>{categoriesItems}</>;
