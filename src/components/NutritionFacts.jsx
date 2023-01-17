941fff8b1850a09a13d6173ae9eb3f450edb0967
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-01-17 22:50:49 +0100

    fix issue in table, change data in json file, add style to map

--- src/components/NutritionFacts.jsx
+++ src/components/NutritionFacts.jsx
@@ -2,19 +2,24 @@ export default function NutritionFacts() {
   return (
     <table className="nutrition-facts-table">
       <tr>
-        <td>Calories</td> <td>466</td>
+        <td>Calories</td>
+        <td>466</td>
       </tr>
       <tr>
-        <td>Total Fat</td> <td>15%</td>
+        <td>Total Fat</td>
+        <td>15%</td>
       </tr>
       <tr>
-        <td>Cholesterol</td> <td>0%</td>
+        <td>Cholesterol</td>
+        <td>0%</td>
       </tr>
       <tr>
-        <td>Sodium</td> <td>1%</td>
+        <td>Sodium</td>
+        <td>1%</td>
       </tr>
       <tr>
-        <td>TotalCarbohydrate</td> <td>28%</td>
+        <td>TotalCarbohydrate</td>
+        <td>28%</td>
       </tr>
       <tr>
         <td>Protein</td> <td>33%</td>
