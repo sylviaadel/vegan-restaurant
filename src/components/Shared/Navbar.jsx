7020cee29c14a01f229f8fc2f1b4e67a494e7598
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-01-19 00:42:00 +0100

    add links to json file

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,22 +1,20 @@
 import { Link } from "react-router-dom";
+import categories from "../../Data/categories.json";
 import mainLogo from "../../assets/images/logo.png";
 
 export default function Navbar() {
+  const categoriesList = categories.map((category) => (
+    <li key={category.id}>
+      <Link to={category.link}>{category.name}</Link>
+    </li>
+  ));
   return (
     <nav className="navbar">
       <Link to="/">
         <img src={mainLogo} alt="A fork and a spoon along with a leaf." />
       </Link>
       <ul>
-        <li>
-          <Link to="/Category/Dishes">Dishes</Link>
-        </li>
-        <li>
-          <Link to="/Category/Desserts">Desserts</Link>
-        </li>
-        <li>
-          <Link to="/Category/Drinks">Drinks</Link>
-        </li>
+        {categoriesList}
         <li>
           <Link to="/Contact">Contact</Link>
         </li>
