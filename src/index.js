8b0b79cba47d33edfaa2380da8e37ab0662adb31
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-01-19 00:35:48 +0100

    code enhancement

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
+import ScrollToTop from "./components/Shared/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
